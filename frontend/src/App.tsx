3e897b872d12a37719d0c439b3b9dea6897385b5
SushilPrajapati1 <soucesil1@gmail.com>
2026-07-16 13:00:33 +0545

    added bugs to fix later hahahahahah

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "sonner";
-import { AuthLayout } from "@/components/layout/AuthLayout";
 import { AppLayout } from "@/components/layout/AppLayout";
+import { AuthLayout } from "@/components/layout/AuthLayout";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
+import Chat from "@/pages/Chat";
 import Login from "@/pages/Login";
+import Profile from "@/pages/Profile";
 import Register from "@/pages/Register";
 import VerifyEmail from "@/pages/VerifyEmail";
-import Chat from "@/pages/Chat";
-import Profile from "@/pages/Profile";
-import Home from "./pages/Home";
-import { useInitAuth } from "./hooks/useInitAuth";
-import { useAuthStore } from "./store/authStore";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
 import { PublicRoute } from "./components/PublicRoute";
-import { useSocket } from "./hooks/useSocket";
-import { SocketContext } from "@/context/SocketContext";
+import { SocketProvider } from "./context/socketProvider";
+import { useInitAuth } from "./hooks/useInitAuth";
+import { useSocketNotifications } from "./hooks/useSocketNotifications";
+import Home from "./pages/Home";
+import { useAuthStore } from "./store/authStore";
 
 const queryClient = new QueryClient();
 
 function AppInner() {
   useInitAuth();
-  const socket = useSocket();
-  console.log(socket, "inner app socket");
+
+  useSocketNotifications();
 
   const isLoading = useAuthStore((state) => state.isLoading);
 
@@ -34,25 +34,23 @@ function AppInner() {
   }
 
   return (
-    <SocketContext.Provider value={socket}>
-      <Routes>
-        <Route element={<PublicRoute />}>
-          <Route element={<AuthLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/verify-email" element={<VerifyEmail />} />
-          </Route>
+    <Routes>
+      <Route element={<PublicRoute />}>
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
         </Route>
-        <Route element={<ProtectedRoute />}>
-          <Route element={<AppLayout />}>
-            <Route path="/chat" element={<Chat />} />
-            <Route path="/chat/:conversationId" element={<Chat />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route element={<AppLayout />}>
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/chat/:conversationId" element={<Chat />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
-      </Routes>
-    </SocketContext.Provider>
+      </Route>
+    </Routes>
   );
 }
 
@@ -61,9 +59,11 @@ function App() {
     <>
       <Toaster theme="dark" />
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <AppInner />
-        </BrowserRouter>
+        <SocketProvider>
+          <BrowserRouter>
+            <AppInner />
+          </BrowserRouter>
+        </SocketProvider>
       </QueryClientProvider>
     </>
   );
